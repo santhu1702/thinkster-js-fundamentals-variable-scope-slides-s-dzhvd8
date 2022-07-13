f208fcd3b585dc2a10f47847af602cb7d087cae6
santhu1702 <santhoshkrishna1702@gmail.com>
2022-07-13 13:57:59 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Variable Scope</h1>`;
 
 // Global Scope
+
+var name = 'santhosh';
+console.log(name);
+
+function hi() {
+  console.log(`hi ${name}`);
+}
+hi();
